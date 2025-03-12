e180ebb4cf9cddd8986a9c74f80224df57ba0bf5
bt00000 <bobthecat1020@gmail.com>
2025-03-12 16:07:36 -0700

    VPC, Route Table, Subnets, NAT Gateway, Internet Gateway (formatted)

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ terraform {
 
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 5.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
@@ -30,7 +30,7 @@ resource "aws_iam_role" "eks_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "eks_role_policy" {
-  role = aws_iam_role.eks_role.name
+  role       = aws_iam_role.eks_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
@@ -86,8 +86,8 @@ resource "aws_route_table" "private_rt" {
 
 # Associate Route Table with Private Subnets
 resource "aws_route_table_association" "private" {
-  count = 2
-  subnet_id = aws_subnet.private[count.index].id
+  count          = 2
+  subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private_rt.id
 }
 
@@ -100,43 +100,45 @@ resource "aws_route_table_association" "public" {
 # Create Private Subnets (For Worker Nodes)
 resource "aws_subnet" "private" {
   count             = 2
-  vpc_id           = aws_vpc.main.id
-  cidr_block       = "10.0.${count.index}.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.${count.index}.0/24"
   availability_zone = element(["us-east-2a", "us-east-2b"], count.index)
 
   tags = {
-    Name                               = "eks-private-subnet-${count.index}"
-    "kubernetes.io/role/internal-elb"  = "1"  # Required for EKS
-    "kubernetes.io/cluster/my-eks-cluster" = "owned"  # Required for EKS
+    Name                                   = "eks-private-subnet-${count.index}"
+    "kubernetes.io/role/internal-elb"      = "1"     # Required for EKS
+    "kubernetes.io/cluster/my-eks-cluster" = "owned" # Required for EKS
   }
 }
 
 # Create Public Subnet (For Load Balancers)
 resource "aws_subnet" "public" {
   count             = 1
-  vpc_id           = aws_vpc.main.id
-  cidr_block       = "10.0.10.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.10.0/24"
   availability_zone = "us-east-2a"
 
   tags = {
-    Name                              = "eks-public-subnet"
-    "kubernetes.io/role/elb"          = "1"  # Required for External LB
-    "kubernetes.io/cluster/my-eks-cluster" = "owned"  # Required for EKS
+    Name                                   = "eks-public-subnet"
+    "kubernetes.io/role/elb"               = "1"     # Required for External LB
+    "kubernetes.io/cluster/my-eks-cluster" = "owned" # Required for EKS
   }
 }
 
-# Create EKS Cluster (With Only Private Subnets)
+# Create EKS Cluster (With Only Private Subnets) & Security Group
 resource "aws_eks_cluster" "eks" {
   name     = var.cluster_name
   role_arn = aws_iam_role.eks_role.arn
   version  = "1.29"
 
   vpc_config {
-    subnet_ids = aws_subnet.private[*].id  # Use only private subnets for EKS security
+    subnet_ids         = aws_subnet.private[*].id
+    security_group_ids = [aws_security_group.eks_cluster_sg.id] # Add security group
   }
 
   depends_on = [aws_iam_role_policy_attachment.eks_role_policy]
 }
+
 
 # Create IAM Role for Worker Nodes
 resource "aws_iam_role" "node_role" {
@@ -155,26 +157,89 @@ resource "aws_iam_role" "node_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "node_role_policy" {
-  role = aws_iam_role.node_role.name
+  role       = aws_iam_role.node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
 resource "aws_iam_role_policy_attachment" "cni_policy" {
-  role = aws_iam_role.node_role.name
+  role       = aws_iam_role.node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+# Create Security Group for EKS Cluster
+resource "aws_security_group" "eks_cluster_sg" {
+  name_prefix = "eks-cluster-sg"
+  description = "Security group for EKS cluster"
+  vpc_id      = aws_vpc.main.id
+
+  # Allow Kubernetes API access
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow worker nodes to communicate with the cluster
+  ingress {
+    from_port       = 10250
+    to_port         = 10250
+    protocol        = "tcp"
+    security_groups = [aws_security_group.eks_node_sg.id]
+  }
+
+  # Allow outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create Security Group for Worker Nodes
+resource "aws_security_group" "eks_node_sg" {
+  name_prefix = "eks-node-sg"
+  description = "Security group for worker nodes"
+  vpc_id      = aws_vpc.main.id
+
+  # Allow all traffic between worker nodes
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    self      = true
+  }
+
+  # Allow outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 # Create Auto Scaling Group for Worker Nodes
 resource "aws_eks_node_group" "node_group" {
-  cluster_name = aws_eks_cluster.eks.name
+  cluster_name  = aws_eks_cluster.eks.name
   node_role_arn = aws_iam_role.node_role.arn
-  subnet_ids = aws_subnet.private[*].id
+  subnet_ids    = aws_subnet.private[*].id
+  ami_type      = "AL2_x86_64"
+  capacity_type = "ON_DEMAND"
 
   scaling_config {
     desired_size = var.node_count
-    min_size = 1
-    max_size = 3
+    min_size     = 1
+    max_size     = 3
   }
 
   instance_types = [var.node_instance_type]
+
+  tags = {
+    "Name"                                 = "eks-node-group"
+    "kubernetes.io/cluster/my-eks-cluster" = "owned" # Required for auto-scaling
+  }
 }
+
+
