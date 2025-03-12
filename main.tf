fb4a3d8dc184809af21985338fa81333a77e02c4
bt00000 <bobthecat1020@gmail.com>
2025-03-12 14:25:42 -0700

    Create IAM Role for EKS & EKS Cluster

--- main.tf
+++ main.tf
@@ -11,3 +11,37 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
+# Create IAM Role for EKS
+resource "aws_iam_role" "eks_role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_role_policy" {
+  role = aws_iam_role.eks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+# Create EKS Cluster
+resource "aws_eks_cluster" "eks" {
+  name = var.cluster_name
+  role_arn = aws_iam_role.eks_role.arn
+  version = "1.29"
+
+  vpc_config {
+    subnet_ids = aws_subnet.private[*].subnet_ids
+  }
+
+  depends_on = [ aws_iam_role_policy_attachment.eks_role_policy ]
+}
