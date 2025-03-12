a276171411145fa8af7b9197d18fb52fa94ca62e
bt00000 <bobthecat1020@gmail.com>
2025-03-12 14:34:00 -0700

    Create IAM Role for Worker Nodes & Auto Scaling Groups

--- main.tf
+++ main.tf
@@ -45,3 +45,44 @@ resource "aws_eks_cluster" "eks" {
 
   depends_on = [ aws_iam_role_policy_attachment.eks_role_policy ]
 }
+
+# Create IAM Role for Worker Nodes
+resource "aws_iam_role" "node_role" {
+  name = "eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "node_role_policy" {
+  role = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam:aws:policy/AmazonAKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "cni_policy" {
+  role = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+# Create Auto Scaling Group for Worker Nodes
+resource "aws_eks_node_group" "node_group" {
+  cluster_name = aws_eks_cluster.eks.name
+  node_role_arn = aws_iam_role.node_role.arn
+  subnet_ids = aws_subnet.private[*].subnet_ids
+
+  scaling_config {
+    desired_size = var.node_count
+    min_size = 1
+    max_size = 3
+  }
+
+  instance_types = [var.node_instance_type]
+}
